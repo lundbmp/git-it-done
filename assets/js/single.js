17480dc21133c33d1154e90b7bcf32f77ee6d77f
lundbmp <m.lundberg008@gmail.com>
2022-05-30 19:33:57 -0500

    display if there are more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issuesContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +8,11 @@ var getRepoIssues = function(repo) {
         if(response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with the request!");
@@ -47,5 +52,18 @@ var displayIssues = function (issues) {
     }
 }
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-getRepoIssues("lundbmp/git-it-done");
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);  
+};
+
+
+getRepoIssues("facebook/react");
